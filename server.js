222aaad9a8b19a17f19010918ba5fd0057b02c2c
TeonaZav <teona.zavrashvili@gmail.com>
2022-10-24 21:02:32 +0400

    change db and gitignore files

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-// const pool = require("./db");
-const PORT = 5000;
+const pool = require("./db");
+require("dotenv").config();
 
+// const PORT = 5000;
+const PORT = process.env.PORT;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
