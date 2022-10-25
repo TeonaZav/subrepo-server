4aada37a6097434e4eb3a3ac6d57b9dab2c6d27f
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2022-10-25 19:44:08 +0400

    add connection string

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+const devConfig = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
+const proConfig = process.env.DATABASE_URL;
+
 const pool = new Pool({
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  password: process.env.DATABASE_PASSWORD,
-  user: process.env.DATABASE_USER,
-  port: process.env.DATABASE_PORT,
+  connectionString:
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 module.exports = pool;
