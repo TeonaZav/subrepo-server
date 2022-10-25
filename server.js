fb7e69d462176a087aa8fb35a409ca605b0cab75
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2022-10-25 19:45:03 +0400

    change port

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const pool = require("./db");
 require("dotenv").config();
-
-// const PORT = 5000;
-const PORT = process.env.PORT;
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
